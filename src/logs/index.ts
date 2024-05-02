eb69068fa289f01b543174dde7db4def0029b17b
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-05-02 11:43:20 +0000

    chore: implemented resumeAfter safety on change streams

--- src/logs/index.ts
+++ src/logs/index.ts
@@ -2,47 +2,49 @@ import { AuthorModel } from "../models/author.model";
 import { BookModel } from "../models/book.model";
 import { LogModel } from "../models/log.model";
 
+function generateLog(data: any) { // To make it truly typesafe, install the mongodb package and import type 'ChangeStreamDocument' then use generics to get the correct doc type.
+  return {
+    item: data.documentKey._id,
+    itemType: `${data.ns.db}.${data.ns.coll}`,
+    action: data.fullDocument.lastUpdatedData.action,
+    actionType: data.fullDocument.lastUpdatedData.actionType,
+    operationType: data.operationType,
+    user: data.fullDocument.lastUpdatedData.user,
+    changes: data.updateDescription,
+    description: data.fullDocument.lastUpdatedData.description,
+    createdAt: new Date(),
+    lastUpdatedMeta: {
+      ...data.fullDocument.lastUpdatedMeta,
+      resumeToken: data._id,
+    }
+  }
+}
+
 
 export async function startLogging(){
 
+    const latestAuthorLog = await LogModel.find({ itemType: 'amazon.authors' }).sort({ 'createdAt': 'desc'}).limit(1);
+    const latestBookLog = await LogModel.find({ itemType: 'amazon.books' }).sort({ 'createdAt': 'desc'}).limit(1);
+    
+
     AuthorModel.watch([], {
         fullDocument: 'updateLookup',
-        fullDocumentBeforeChange: 'whenAvailable'
+        fullDocumentBeforeChange: 'whenAvailable',
+        resumeAfter: latestAuthorLog[0].lastUpdatedMeta?.resumeToken,
     }).
     on('change',async (data) => {
-        console.log(data);
-        console.log('\nlastUpdated: ', data.fullDocument.lastUpdatedData)
-        await LogModel.insertMany({
-            item: data.documentKey._id,
-            itemType: `${data.ns.db}.${data.ns.coll}`,
-            action: data.fullDocument.lastUpdatedData.action,
-            actionType: data.fullDocument.lastUpdatedData.actionType,
-            operationType: data.operationType,
-            user: data.fullDocument.lastUpdatedData.user,
-            changes: data.updateDescription,
-            description: data.fullDocument.lastUpdatedData.description,
-            lastUpdatedMeta: data.fullDocument.lastUpdatedMeta
-        });
-        
+       const doc = await LogModel.insertMany(generateLog(data));
+       console.log(doc) 
     })
 
     BookModel.watch([], {
         fullDocument: 'updateLookup',
-        fullDocumentBeforeChange: 'whenAvailable'
+        fullDocumentBeforeChange: 'whenAvailable',
+        resumeAfter: latestBookLog[0].lastUpdatedMeta?.resumeToken,
     })
     .on('change', async (data) => {
-        console.log(data);
-        await LogModel.insertMany({
-            item: data.documentKey._id,
-            itemType: `${data.ns.db}.${data.ns.coll}`,
-            action: data.fullDocument.lastUpdatedData.action,
-            actionType: data.fullDocument.lastUpdatedData.actionType,
-            operationType: data.operationType,
-            user: data.fullDocument.lastUpdatedData.user,
-            changes: data.updateDescription,
-            description: data.fullDocument.lastUpdatedData.description,
-            lastUpdatedMeta: data.fullDocument.lastUpdatedMeta
-        });
+        const doc = await LogModel.insertMany(generateLog(data));
+        console.log(doc)
     })
 
 }
